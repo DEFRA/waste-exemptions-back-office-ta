83d60003577721dd0005bab44506a0ff58e61e06
irisfaraway <iris.faraway@gmail.com>
2019-01-17 14:00:56 +0000

    Users must be logged in to access back office
    If a user who is not logged in attempts to access pages in the host app or the engine, they will be redirected to the Devise sign-in pages.

--- spec/requests/dashboards_spec.rb
+++ spec/requests/dashboards_spec.rb
@@ -4,14 +4,27 @@ require "rails_helper"
 
 RSpec.describe "Dashboards", type: :request do
   describe "/" do
-    it "renders the index template" do
-      get "/"
-      expect(response).to render_template(:index)
+    context "when a valid user is signed in" do
+      before { sign_in(create(:user)) }
+
+      it "renders the index template" do
+        get "/"
+        expect(response).to render_template(:index)
+      end
+
+      it "returns a 200 response" do
+        get "/"
+        expect(response).to have_http_status(200)
+      end
     end
 
-    it "returns a 200 response" do
-      get "/"
-      expect(response).to have_http_status(200)
+    context "when a valid user is not signed in" do
+      before { sign_out(create(:user)) }
+
+      it "redirects to the sign-in page" do
+        get "/"
+        expect(response).to redirect_to(new_user_session_path)
+      end
     end
   end
 end
--- spec/requests/engine_spec.rb
+++ spec/requests/engine_spec.rb
@@ -4,9 +4,13 @@ require "rails_helper"
 
 RSpec.describe "Waste Exemptions Engine", type: :request do
   describe "/pages/version" do
-    it "returns a 200 response" do
-      get "/pages/version"
-      expect(response).to have_http_status(200)
+    context "when a valid user is signed in" do
+      before { sign_in(create(:user)) }
+
+      it "returns a 200 response" do
+        get "/pages/version"
+        expect(response).to have_http_status(200)
+      end
     end
   end
 end
