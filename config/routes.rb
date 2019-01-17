13c353202e891767773d4b00fe107eb51bf98319
irisfaraway <iris.faraway@gmail.com>
2019-01-17 13:46:10 +0000

    Implement better session security
    This allows us to invalidate user session cookies after logout, a security fix implemented in waste carriers in https://github.com/DEFRA/waste-carriers-back-office/pull/124. This prevents attackers from repeating requests made while logged in by reusing the cookie.

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,8 @@ Rails.application.routes.draw do
 
   root "dashboards#index"
 
-  devise_for :users
+  devise_for :users,
+             controllers: { sessions: "sessions" },
+             path: "/bo/users",
+             path_names: { sign_in: "sign_in", sign_out: "sign_out" }
 end
