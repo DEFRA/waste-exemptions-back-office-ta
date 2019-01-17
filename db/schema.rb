6c9ab0b5070223ab72bacff7ee521d180c052b94
irisfaraway <iris.faraway@gmail.com>
2019-01-17 18:01:28 +0000

    Add role column to Users table
    This will store the role which determines what permissions the user has.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190116160355) do
+ActiveRecord::Schema.define(version: 20190117175641) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -202,6 +202,7 @@ ActiveRecord::Schema.define(version: 20190116160355) do
     t.string   "session_token"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "role"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
