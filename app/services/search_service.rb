27bafc11b6d352770c047247bf1935c5707d3e45
irisfaraway <iris.faraway@gmail.com>
2019-01-28 17:22:26 +0000

    Return no results if no search term is given
    We only want to see results when a user enters a search term. This also saves us from hitting the DB.

--- app/services/search_service.rb
+++ app/services/search_service.rb
@@ -4,6 +4,8 @@ class SearchService
   def initialize; end
 
   def search(term, page)
+    return Kaminari.paginate_array([]).page(page) if term.blank?
+
     WasteExemptionsEngine::Registration.search_term(term).page(page)
   end
 end
--- spec/services/search_service_spec.rb
+++ spec/services/search_service_spec.rb
@@ -12,11 +12,21 @@ RSpec.describe SearchService do
     service.search(term, 1)
   end
 
-  it "should return matching registrations" do
-    expect(results).to include(registration)
+  context "when a search term is provided" do
+    it "should return matching registrations" do
+      expect(results).to include(registration)
+    end
+
+    it "should not return non-matching registrations" do
+      expect(results).to_not include(other_registration)
+    end
   end
 
-  it "should not return non-matching registrations" do
-    expect(results).to_not include(other_registration)
+  context "when no search term is provided" do
+    let(:term) { nil }
+
+    it "should return no results" do
+      expect(results.length).to eq(0)
+    end
   end
 end
