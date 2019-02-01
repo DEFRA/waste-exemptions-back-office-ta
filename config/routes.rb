fe4df38b07aa8b14d8df5569e5ee10a004990b9e
irisfaraway <iris.faraway@gmail.com>
2019-02-01 14:49:25 +0000

    Mount engine last in routes
    Otherwise we run into clashing issues when we try to add more routes to the host app - it redirects to an error page instead.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  mount WasteExemptionsEngine::Engine => "/"
-
   root "dashboards#index"
 
   devise_for :users,
              controllers: { sessions: "sessions" },
              path: "/users",
              path_names: { sign_in: "sign_in", sign_out: "sign_out" }
+
+  mount WasteExemptionsEngine::Engine => "/"
 end
