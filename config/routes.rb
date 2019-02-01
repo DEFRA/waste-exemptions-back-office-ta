90b86cfa0b2f4e4d56496edfea3dcd081a87485b
irisfaraway <iris.faraway@gmail.com>
2019-02-01 12:26:49 +0000

    Remove /bo from Devise paths
    This was needed in waste carriers as we had to run two systems concurrently - however this won't be an issue for WEX so we can simplify the URL.

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
 
   devise_for :users,
              controllers: { sessions: "sessions" },
-             path: "/bo/users",
+             path: "/users",
              path_names: { sign_in: "sign_in", sign_out: "sign_out" }
 end
