2d8ca0fcfb4f92237b2dbd5fe0bf9a7ba54fc360
irisfaraway <iris.faraway@gmail.com>
2019-02-05 14:39:55 +0000

    Add activate and deactivate pages
    This commit adds a new controller for managing the activation and deactivation of users. Activating or deactivating a user goes through a confirmation page before the change is made. Attempting to access the page for an impossible action (eg deactivating a user who has already been deactivated) will redirect to the users list instead.
    
    The actual activating and deactivating will be added in the next commit.

---
+++ app/controllers/user_activations_controller.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+class UserActivationsController < ApplicationController
+  def activate_form
+    assign_user(params[:id])
+    redirect_to users_url if @user.active?
+  end
+
+  def deactivate_form
+    assign_user(params[:id])
+    redirect_to users_url unless @user.active?
+  end
+
+  def activate
+    assign_user(params[:id])
+    redirect_to users_url
+  end
+
+  def deactivate
+    assign_user(params[:id])
+    redirect_to users_url
+  end
+
+  private
+
+  def assign_user(id)
+    @user = User.find(id)
+  end
+end
---
+++ spec/requests/user_activations_spec.rb
@@ -0,0 +1,100 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "User Activations", type: :request do
+  let(:active_user) { create(:user) }
+  let(:inactive_user) { create(:user, :inactive) }
+
+  describe "GET /users/activate/:id" do
+    context "when a system user is signed in" do
+      let(:user) { create(:user, :system) }
+      before(:each) do
+        sign_in(user)
+      end
+
+      context "when the user to be activated is inactive" do
+        it "renders the activate_form template" do
+          get "/users/activate/#{inactive_user.id}"
+          expect(response).to render_template(:activate_form)
+        end
+      end
+
+      context "when the user to be activated is already active" do
+        it "redirects to the user list" do
+          get "/users/activate/#{active_user.id}"
+          expect(response).to redirect_to(users_path)
+        end
+      end
+    end
+  end
+
+  describe "GET /users/deactivate/:id" do
+    context "when a system user is signed in" do
+      let(:user) { create(:user, :system) }
+      before(:each) do
+        sign_in(user)
+      end
+
+      context "when the user to be deactivated is active" do
+        it "renders the deactivate_form template" do
+          get "/users/deactivate/#{active_user.id}"
+          expect(response).to render_template(:deactivate_form)
+        end
+      end
+
+      context "when the user to be deactivated is already inactive" do
+        it "redirects to the user list" do
+          get "/users/deactivate/#{inactive_user.id}"
+          expect(response).to redirect_to(users_path)
+        end
+      end
+    end
+  end
+
+  describe "POST /users/activate" do
+    context "when a system user is signed in" do
+      let(:user) { create(:user, :system) }
+      before(:each) do
+        sign_in(user)
+      end
+
+      context "when the user to be activated is inactive" do
+        it "redirects to the user list" do
+          post "/users/activate/#{inactive_user.id}"
+          expect(response).to redirect_to(users_path)
+        end
+      end
+
+      context "when the user to be activated is already active" do
+        it "redirects to the user list" do
+          post "/users/activate/#{active_user.id}"
+          expect(response).to redirect_to(users_path)
+        end
+      end
+    end
+  end
+
+  describe "POST /users/deactivate" do
+    context "when a system user is signed in" do
+      let(:user) { create(:user, :system) }
+      before(:each) do
+        sign_in(user)
+      end
+
+      context "when the user to be deactivated is active" do
+        it "redirects to the user list" do
+          post "/users/deactivate/#{active_user.id}"
+          expect(response).to redirect_to(users_path)
+        end
+      end
+
+      context "when the user to be deactivated is already inactive" do
+        it "redirects to the user list" do
+          post "/users/deactivate/#{inactive_user.id}"
+          expect(response).to redirect_to(users_path)
+        end
+      end
+    end
+  end
+end
