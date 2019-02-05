53a0cab8a0a48c49783d5c8d41470b3efb9dd5a5
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2019-02-05 15:03:19 +0000

    Align rspec test setup with WEX engine
    Whilst working on the engine we have found issues with the initial Database cleaner setup, as well as how simplecov was configured.
    
    We have also settled on a pattern where any test dependencies are required and configured in separate support files to keep the content of the `spec` and `rails` helpers to a minimum.
    
    This change rectifies the issues with Database cleaner and simplecov, and implements the pattern, by aligning how rspec is setup to match the WEX engine.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,10 +4,20 @@
 require "spec_helper"
 ENV["RAILS_ENV"] ||= "test"
 require File.expand_path("../config/environment", __dir__)
+
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require "rspec/rails"
 # Add additional requires below this line. Rails is not loaded until this point!
+
+# Checks for pending migrations and applies them before tests are run.
+# If you are not using ActiveRecord, you can remove these lines.
+begin
+  ActiveRecord::Migration.maintain_test_schema!
+rescue ActiveRecord::PendingMigrationError => e
+  puts e.to_s.strip
+  exit 1
+end
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -17,12 +27,10 @@ require "rspec/rails"
 # end with _spec.rb. You can configure this pattern with the --pattern
 # option on the command line or in ~/.rspec, .rspec or `.rspec-local`.
 #
-# The following line is provided for convenience purposes. It has the downside
-# of increasing the boot-up time by auto-requiring all files in the support
-# directory. Alternatively, in the individual `*_spec.rb` files, manually
-# require only the support files necessary.
-
-Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+# We make an exception for simplecov because that will already have been
+# required and run at the very top of spec_helper.rb
+support_files = Dir["./spec/support/**/*.rb"].reject { |file| file == "./spec/support/simplecov.rb" }
+support_files.each { |f| require f }
 
 RSpec.configure do |config|
   config.include Devise::Test::IntegrationHelpers, type: :request
@@ -46,10 +54,4 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
-
-  # Clean the database before running tests
-  config.before(:suite) do
-    DatabaseCleaner.strategy = :truncation
-    DatabaseCleaner.clean
-  end
 end
