0e60d1bca3df0f51f66c6a94ee9421b0f45a4116
irisfaraway <iris.faraway@gmail.com>
2019-02-12 14:28:47 +0000

    Add specs for inviting users
    This functionality is provided by the devise_invitable gem. However, getting these specs in place now means it will be easier to test our upcoming modifications to its behaviour.

---
+++ spec/requests/user_invitations_spec.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "User Invitations", type: :request do
+  describe "GET /users/invitation/new" do
+    context "when a system user is signed in" do
+      let(:user) { create(:user, :system) }
+      before(:each) do
+        sign_in(user)
+      end
+
+      it "renders the new template" do
+        get "/users/invitation/new"
+        expect(response).to render_template(:new)
+      end
+    end
+  end
+
+  describe "POST /users/invitation" do
+    context "when a system user is signed in" do
+      let(:user) { create(:user, :system) }
+      before(:each) do
+        sign_in(user)
+      end
+
+      let(:params) do
+        { user: { email: attributes_for(:user)[:email] } }
+      end
+
+      it "redirects to the root path" do
+        post "/users/invitation", params
+        expect(response).to redirect_to(root_path)
+      end
+
+      it "creates a new user" do
+        old_user_count = User.count
+
+        post "/users/invitation", params
+        expect(User.count).to eq(old_user_count + 1)
+      end
+    end
+  end
+end
