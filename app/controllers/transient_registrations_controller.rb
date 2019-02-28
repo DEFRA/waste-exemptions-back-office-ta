1049c7e7d7b84abc1a61aaaed7978a1aab2ac370
irisfaraway <iris.faraway@gmail.com>
2019-02-28 11:05:38 +0000

    Display info text when missing contact or applicant data
    When a user is early in the registration journey, the transient_registration is missing a lot of data. This commit adds some helper methods to check if any data is present for a section and display some info text if not, rather than just a lonely heading.

--- app/controllers/transient_registrations_controller.rb
+++ app/controllers/transient_registrations_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class TransientRegistrationsController < ApplicationController
+  helper RegistrationsHelper
+
   def show
     find_resource(params[:reference])
     authorize
