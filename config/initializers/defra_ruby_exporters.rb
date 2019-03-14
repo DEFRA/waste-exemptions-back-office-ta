89f73ed0300c3e36c2059ab63148069c60718ca3
Edward Minnett <edward.minnett@defra.gov.uk>
2019-03-14 16:06:31 +0000

    Fix a copy paste error

--- config/initializers/defra_ruby_exporters.rb
+++ config/initializers/defra_ruby_exporters.rb
@@ -5,8 +5,8 @@ module DefraRuby
     AWS_REGION = (ENV["AWS_REGION"] || "eu-west-1").freeze
 
     EPR_EXPORT_AWS_CREDENTIALS = Aws::Credentials.new(
-      (ENV["AWS_DAILY_EXPORT_ACCESS_KEY_ID"] || raise("Environment variable AWS_MANUAL_EXPORT_ACCESS_KEY_ID has not been set")),
-      (ENV["AWS_DAILY_EXPORT_SECRET_ACCESS_KEY"] || raise("Environment variable AWS_MANUAL_EXPORT_SECRET_ACCESS_KEY has not been set"))
+      (ENV["AWS_DAILY_EXPORT_ACCESS_KEY_ID"] || raise("Environment variable AWS_DAILY_EXPORT_ACCESS_KEY_ID has not been set")),
+      (ENV["AWS_DAILY_EXPORT_SECRET_ACCESS_KEY"] || raise("Environment variable AWS_DAILY_EXPORT_SECRET_ACCESS_KEY has not been set"))
     )
 
     EPR_EXPORT_S3_BUCKET = (ENV["AWS_DAILY_EXPORT_BUCKET"] || raise("Environment variable AWS_DAILY_EXPORT_BUCKET has not been set")).freeze
