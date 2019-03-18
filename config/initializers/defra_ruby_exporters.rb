0d1d414543d1dc89542426d43815e5a369c4f358
Edward Minnett <edward.minnett@defra.gov.uk>
2019-03-18 14:49:08 +0000

    Fix Rubocop offenses

--- config/initializers/defra_ruby_exporters.rb
+++ config/initializers/defra_ruby_exporters.rb
@@ -7,14 +7,18 @@ module DefraRuby
     AWS_REGION = (ENV["AWS_REGION"] || "eu-west-1")
 
     EPR_EXPORT_AWS_CREDENTIALS = Aws::Credentials.new(
-      (ENV["AWS_DAILY_EXPORT_ACCESS_KEY_ID"] || raise("Environment variable AWS_DAILY_EXPORT_ACCESS_KEY_ID has not been set")),
-      (ENV["AWS_DAILY_EXPORT_SECRET_ACCESS_KEY"] || raise("Environment variable AWS_DAILY_EXPORT_SECRET_ACCESS_KEY has not been set"))
+      (ENV["AWS_DAILY_EXPORT_ACCESS_KEY_ID"] || raise_missing_env_var("AWS_DAILY_EXPORT_ACCESS_KEY_ID")),
+      (ENV["AWS_DAILY_EXPORT_SECRET_ACCESS_KEY"] || raise_missing_env_var("AWS_DAILY_EXPORT_SECRET_ACCESS_KEY"))
     )
 
-    EPR_EXPORT_S3_BUCKET = (ENV["AWS_DAILY_EXPORT_BUCKET"] || raise("Environment variable AWS_DAILY_EXPORT_BUCKET has not been set"))
+    EPR_EXPORT_S3_BUCKET = (ENV["AWS_DAILY_EXPORT_BUCKET"] || raise_missing_env_var("AWS_DAILY_EXPORT_BUCKET"))
 
     EPR_EXPORT_FILENAME = "waste_exemptions_epr_daily_full.csv"
 
     EPR_EXPORT_TIME = (ENV["EXPORT_SERVICE_EPR_EXPORT_TIME"] || "1:05")
+
+    def self.raise_missing_env_var(variable)
+      raise("Environment variable #{variable} has not been set")
+    end
   end
 end
