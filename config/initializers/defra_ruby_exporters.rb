9105b486b110858c345455537767b700bfcbf445
Edward Minnett <edward.minnett@defra.gov.uk>
2019-03-18 11:56:03 +0000

    Add missing `frozen_string_literal` statement

--- config/initializers/defra_ruby_exporters.rb
+++ config/initializers/defra_ruby_exporters.rb
@@ -1,18 +1,20 @@
+# frozen_string_literal: true
+
 module DefraRuby
   module Exporters
-    BATCH_SIZE = (ENV["EXPORT_SERVICE_BATCH_SIZE"] || 3000).to_i.freeze
+    BATCH_SIZE = (ENV["EXPORT_SERVICE_BATCH_SIZE"] || 3000).to_i
 
-    AWS_REGION = (ENV["AWS_REGION"] || "eu-west-1").freeze
+    AWS_REGION = (ENV["AWS_REGION"] || "eu-west-1")
 
     EPR_EXPORT_AWS_CREDENTIALS = Aws::Credentials.new(
       (ENV["AWS_DAILY_EXPORT_ACCESS_KEY_ID"] || raise("Environment variable AWS_DAILY_EXPORT_ACCESS_KEY_ID has not been set")),
       (ENV["AWS_DAILY_EXPORT_SECRET_ACCESS_KEY"] || raise("Environment variable AWS_DAILY_EXPORT_SECRET_ACCESS_KEY has not been set"))
     )
 
-    EPR_EXPORT_S3_BUCKET = (ENV["AWS_DAILY_EXPORT_BUCKET"] || raise("Environment variable AWS_DAILY_EXPORT_BUCKET has not been set")).freeze
+    EPR_EXPORT_S3_BUCKET = (ENV["AWS_DAILY_EXPORT_BUCKET"] || raise("Environment variable AWS_DAILY_EXPORT_BUCKET has not been set"))
 
     EPR_EXPORT_FILENAME = "waste_exemptions_epr_daily_full.csv"
 
-    EPR_EXPORT_TIME = (ENV["EXPORT_SERVICE_EPR_EXPORT_TIME"] || "1:05").freeze 
+    EPR_EXPORT_TIME = (ENV["EXPORT_SERVICE_EPR_EXPORT_TIME"] || "1:05")
   end
 end
