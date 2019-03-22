1d825cf70607d6057bbe456d705eda1b27688452
Edward Minnett <edward.minnett@defra.gov.uk>
2019-03-22 15:44:04 +0000

    Refactor DeregisterExemptionsController

--- app/controllers/deregister_exemptions_controller.rb
+++ app/controllers/deregister_exemptions_controller.rb
@@ -14,18 +14,13 @@ class DeregisterExemptionsController < ApplicationController
 
     deregistration_service = DeregistrationService.new(current_user, @resource)
 
-    if deregistration_service.deregistration_allowed?
-      if @deregister_exemptions_form.submit(params[:deregister_exemptions_form])
-        deregistration_service.deregister!(@deregister_exemptions_form.state_transition)
-        @resource.update_attributes(deregistration_message: @deregister_exemptions_form.message)
-        successful_redirection = WasteExemptionsEngine::ApplicationController::SUCCESSFUL_REDIRECTION_CODE
-        redirect_to registration_path(reference: @resource.registration.reference), status: successful_redirection
-      else
-        render :new
-        false
-      end
+    return handle_permission_denied unless deregistration_service.deregistration_allowed?
+
+    if @deregister_exemptions_form.submit(params[:deregister_exemptions_form])
+      process_form_submission(deregistration_service)
     else
-      handle_permission_denied
+      render :new
+      false
     end
   end
 
@@ -38,5 +33,12 @@ class DeregisterExemptionsController < ApplicationController
   def handle_permission_denied
     unsuccessful_redirection = WasteExemptionsEngine::ApplicationController::UNSUCCESSFUL_REDIRECTION_CODE
     redirect_to "/pages/permission", status: unsuccessful_redirection
+  end
+
+  def process_form_submission(deregistration_service)
+    deregistration_service.deregister!(@deregister_exemptions_form.state_transition)
+    @resource.update_attributes(deregistration_message: @deregister_exemptions_form.message)
+    successful_redirection = WasteExemptionsEngine::ApplicationController::SUCCESSFUL_REDIRECTION_CODE
+    redirect_to registration_path(reference: @resource.registration.reference), status: successful_redirection
   end
 end
