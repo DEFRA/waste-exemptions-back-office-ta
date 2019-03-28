f3dd4c43d7b94a94396e112170536b7a76e456d7
Edward Minnett <edward.minnett@defra.gov.uk>
2019-03-28 17:14:38 +0000

    Update and refactor the export service

--- lib/defra_ruby/exporters/registration_export_service.rb
+++ lib/defra_ruby/exporters/registration_export_service.rb
@@ -2,31 +2,57 @@
 
 require "csv"
 require_relative "registration_epr_export_report"
+require_relative "bulk_export_file"
 
 module DefraRuby
   module Exporters
-    class RegistrationExportService
+    module RegistrationExportService
 
-      def epr_export
-        file_path = full_path(EPR_EXPORT_FILENAME)
+      def self.epr_export
+        config.ensure_valid
+        file_path = full_path("#{config.epr_export_filename}.csv")
         File.new(file_path, "wb")
         write_to_file(RegistrationEprExportReport, file_path)
         save_to_s3(:epr, file_path)
         File.delete(file_path) if File.exist? file_path
       end
 
-      def write_to_file(report_class, file_path)
+      def self.bulk_export
+        config.ensure_valid
+        oldest = RegistrationEprExportReport.query.first.registered_on
+        newest = RegistrationEprExportReport.query.last.registered_on
+        date_ranges = date_range_helper.generate_date_ranges(oldest, newest, config.bulk_export_number_of_months)
+
+        # Clear the existing exports
+        export_bucket(:bulk).clear!
+        DefraRuby::Exporters::BulkExportFile.destroy_all
+
+        date_ranges.each do |date_range|
+          date_range_description = date_range_helper.describe_date_range(date_range)
+          file_name = "#{config.bulk_export_filename_base}_#{date_range_description}.csv"
+          file_path = full_path(file_name)
+          File.new(file_path, "wb")
+          write_to_file(RegistrationBulkExportReport, file_path, filter: { date_range: date_range })
+          save_to_s3(:bulk, file_path)
+          File.delete(file_path) if File.exist? file_path
+          DefraRuby::Exporters::BulkExportFile.create(file_name: file_name)
+        end
+      end
+
+      def self.write_to_file(report_class, file_path, options = {})
         exporter_name = report_class.name.demodulize
         Rails.logger.info "Started writing #{file_path} from #{exporter_name}"
 
         headers = report_class::COLUMNS.map { |c| c[:header] }
         attributes = report_class::COLUMNS.map { |c| c[:attribute] }
 
+        data_query = options[:filter].present? ? report_class.query(options[:filter]) : report_class.query
+
         CSV.open(file_path, "wb", force_quotes: true) do |csv|
           csv.flush
           csv << headers
 
-          report_class.query.find_in_batches(batch_size: BATCH_SIZE) do |batch|
+          data_query.find_in_batches(batch_size: config.batch_size.to_i) do |batch|
             batch.each do |record|
               # Evaluate the chained messages that define each attribute on rep
               csv << attributes.map { |a| a.split(".").reduce(record, :try) }
@@ -39,7 +65,7 @@ module DefraRuby
         Rails.logger.info "Finished writing #{file_path} from #{exporter_name}"
       end
 
-      def save_to_s3(export_type, file_path)
+      def self.save_to_s3(export_type, file_path)
         Rails.logger.info "Started daily upload of #{file_path} to s3"
 
         s3_object = export_bucket(export_type).object(File.basename(file_path))
@@ -50,19 +76,38 @@ module DefraRuby
         result
       end
 
-      private
+      def self.presigned_url(export_type, file_name)
+        export_bucket(export_type)
+          .object(file_name)
+          .presigned_url(
+            :get,
+            expires_in: 20 * 60, # 20 minutes in seconds
+            secure: true,
+            response_content_type: "text/csv",
+            response_content_disposition: "attachment; filename=#{file_name}"
+          )
+      end
 
-      def full_path(file_name)
+      private_class_method def self.config
+        DefraRuby::Exporters.configuration
+      end
+
+      private_class_method def self.date_range_helper
+        DefraRuby::Exporters::Helpers::DateRange
+      end
+
+      private_class_method def self.full_path(file_name)
         Rails.root.join "tmp", file_name
       end
 
-      def export_bucket(export_type)
+      private_class_method def self.export_bucket(export_type)
         aws_config = {
-          epr: { credentials: EPR_EXPORT_AWS_CREDENTIALS, bucket: EPR_EXPORT_S3_BUCKET }
+          epr: { credentials: config.epr_export_aws_credentials, bucket: config.epr_export_s3_bucket },
+          bulk: { credentials: config.bulk_export_aws_credentials, bucket: config.bulk_export_s3_bucket }
         }
 
         s3 = Aws::S3::Resource.new(
-          region: AWS_REGION,
+          region: config.aws_region,
           credentials: aws_config[export_type][:credentials]
         )
 
