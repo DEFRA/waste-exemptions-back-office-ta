5b066259ac7ab17894e505fea24deafa3d7bc75f
Edward Minnett <edward.minnett@defra.gov.uk>
2019-03-28 17:13:22 +0000

    Refactor registration test helpers

--- spec/support/helpers/registrations.rb
+++ spec/support/helpers/registrations.rb
@@ -4,11 +4,11 @@ module Helpers
   module Registrations
     STANDARD_EXPIRATION = WasteExemptionsEngine.configuration.years_before_expiry.years
 
-    def self.activate(registration)
+    def self.activate(registration, registration_date = Date.today)
       registration.registration_exemptions.each do |registration_exemption|
         registration_exemption.state = "active"
-        registration_exemption.registered_on = Date.today
-        registration_exemption.expires_on = Date.today + (STANDARD_EXPIRATION - 1.day)
+        registration_exemption.registered_on = registration_date
+        registration_exemption.expires_on = registration_date + (STANDARD_EXPIRATION - 1.day)
         registration_exemption.save!
       end
     end
