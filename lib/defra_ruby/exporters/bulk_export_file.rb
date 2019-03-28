cc9b82e04126777a8de373898681e4f66525bf55
Edward Minnett <edward.minnett@defra.gov.uk>
2019-03-28 16:59:08 +0000

    Add BulkExportFile

---
+++ lib/defra_ruby/exporters/bulk_export_file.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+# frozen_string_literal: true
+
+module DefraRuby
+  module Exporters
+    class BulkExportFile < ActiveRecord::Base
+      self.table_name = :defra_ruby_exporters_bulk_export_files
+    end
+  end
+end
