4600bca91d417b59e726e4cf8bf4e916b830ccc8
Edward Minnett <edward.minnett@defra.gov.uk>
2019-04-03 15:39:25 +0100

    Ensure proper ordering of exemptions

--- spec/models/registration_exemption_spec.rb
+++ spec/models/registration_exemption_spec.rb
@@ -10,6 +10,42 @@ RSpec.describe WasteExemptionsEngine::RegistrationExemption, type: :model do
   transitions = deregistration_states + [:expire]
   inactive_states = %i[ceased revoked expired]
 
+  describe "#order_by_state_then_id" do
+    let(:registration) do
+      registration = create(:registration)
+      5.times do
+        %i[active ceased revoked expired].each do |state|
+          create(
+            :registration_exemption,
+            registration: registration,
+            exemption: WasteExemptionsEngine::Exemption.all.sample,
+            state: state
+          )
+        end
+      end
+      registration
+    end
+
+    it "returns the registration exemptions in a specific order of states and then by id" do
+      sorted_registration_exemptions = registration.registration_exemptions.order_by_state_then_id
+      # --- Confirm the States are in the expected order ---
+      # We need to use chunk_while instead of uniq to confirm the states are sorted as uniq could
+      # pass the test but fail the expectation.
+      grouped_states = sorted_registration_exemptions.map(&:state).chunk_while { |a, b| a == b }.to_a
+      expect(grouped_states.count).to eq(4) # This can only be the case if the states are ordered.
+      expect(grouped_states.flatten.uniq).to eq(%w[active ceased revoked expired]) # The correct order
+
+      # --- Confirm the IDs are sequential for each state ---
+      sorted_states_and_ids = sorted_registration_exemptions.map { |re| [re.state, re.id] }
+      # Group the IDs for each state so we confirm the ids for each group are sequential
+      grouped_ids = sorted_states_and_ids.group_by(&:first).map { |_state, state_id_pairs| state_id_pairs.map(&:last) }
+      expect(grouped_ids.count).to eq(4) # Confirm there are the same number of groups as states
+      grouped_ids.each do |ids|
+        expect(ids.sort).to eq(ids)
+      end
+    end
+  end
+
   describe "exemption state" do
     context "when the state is 'active'" do
       before(:each) do
