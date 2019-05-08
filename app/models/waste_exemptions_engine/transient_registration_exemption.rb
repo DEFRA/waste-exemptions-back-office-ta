c2d487ce66581c69dbb1fabdf83488a7d5f9e7ef
cintamani <cintamani.puddu@gmail.com>
2019-05-08 15:52:31 +0100

    fix rubocop issues

--- app/models/waste_exemptions_engine/transient_registration_exemption.rb
+++ app/models/waste_exemptions_engine/transient_registration_exemption.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
-require WasteExemptionsEngine::Engine.root.join("app", "models", "waste_exemptions_engine", "transient_registration_exemption")
+require WasteExemptionsEngine::Engine.root.join(
+  "app",
+  "models",
+  "waste_exemptions_engine",
+  "transient_registration_exemption"
+)
 
 module WasteExemptionsEngine
   class TransientRegistrationExemption < ActiveRecord::Base
