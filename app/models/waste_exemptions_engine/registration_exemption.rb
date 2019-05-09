a2f95a97aa4afdc6cd7ce4e9fb5374203a023752
cintamani <cintamani.puddu@gmail.com>
2019-05-09 14:37:54 +0100

    Fix bug on show of New registration objects (#159)
    * Fix bug on the show of New registration objects
    
    Fixes https://eaflood.atlassian.net/browse/RUBY-329
    
    The issues were related to a missing scope for transient registration exemptions.
    It was only appearing for in-progress registration with already related transient exemptions.
    In order to reproduce the bug on the test suite and prevent regression, exemptions have been added to the base new_registration object.
    The missing scope has been externalised to a concern and included in transient registration exemptions models from the engine.

--- app/models/waste_exemptions_engine/registration_exemption.rb
+++ app/models/waste_exemptions_engine/registration_exemption.rb
@@ -5,17 +5,6 @@ require WasteExemptionsEngine::Engine.root.join("app", "models", "waste_exemptio
 module WasteExemptionsEngine
   class RegistrationExemption < ActiveRecord::Base
     include CanDeactivateExemption
-
-    scope :order_by_state_then_exemption_id, lambda {
-      order(
-        "CASE
-          WHEN state = 'active'  THEN '1'
-          WHEN state = 'ceased'  THEN '2'
-          WHEN state = 'revoked' THEN '3'
-          WHEN state = 'expired' THEN '4'
-        END",
-        :exemption_id
-      )
-    }
+    include CanBeOrderedByStateAndExemptionId
   end
 end
