85cb6ef9e9dba9d5f23b0941e49c8481f0eb2128
cintamani <cintamani.puddu@gmail.com>
2019-05-13 10:59:17 +0100

    Update app/controllers/concerns/waste_exemptions_engine/edit_permission_checks.rb

--- app/controllers/concerns/waste_exemptions_engine/edit_permission_checks.rb
+++ app/controllers/concerns/waste_exemptions_engine/edit_permission_checks.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
-require WasteExemptionsEngine::Engine.root.join("app", "controllers", "concerns", "waste_exemptions_engine", "edit_permission_checks")
+require WasteExemptionsEngine::Engine.root.join(
+  "app",
+  "controllers",
+  "concerns",
+  "waste_exemptions_engine",
+  "edit_permission_checks"
+)
 
 module WasteExemptionsEngine
   module EditPermissionChecks
