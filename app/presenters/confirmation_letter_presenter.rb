8603f3e5f43ccdbc5a3404108a2bc7bb5e56ce8b
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2019-05-15 22:06:10 +0100

    Adopt base_presenter delegation pattern for letter
    In other projects we have use a `base_presenter` essentially as a way of merging the attributes of the source model and the presenter.
    
    Prior to this change `ConfirmationLetterPresenter` held the registration as an attribute, and explicitly delegated calls to `reference` to it.
    
    As we are now intending to make calls to a number of other registration attributes as part of our simplification work we have 3 options
    
    - expose `@registration` as an attribute on the presenter and then call `confirmation_letter_data.registration.some_attribute` in the views
    - add a `delegate :some_attribute, to: :registration` for every attribute we wish to expose
    - adopt the `base_presenter` pattern which uses a `SimpleDelegator`
    
    Obviously with this change I have gone with No. 3, as I believe
    
    - it makes the letter presenter consistent with other projects
    - it is much more flexible and less brittle than option 2
    - it feels cleaner than option 1
    
    Essentially it gets the presenter closer to what I believe the purpose of a presenter is; a resource that holds both the model data and the logic for displaying that model data needed by the view consuming it.

--- app/presenters/confirmation_letter_presenter.rb
+++ app/presenters/confirmation_letter_presenter.rb
@@ -1,11 +1,6 @@
 # frozen_string_literal: true
 
-class ConfirmationLetterPresenter
-  delegate :reference, to: :registration
-
-  def initialize(registration)
-    @registration = registration
-  end
+class ConfirmationLetterPresenter < BasePresenter
 
   def sorted_active_registration_exemptions
     registration_exemptions_with_exemptions.where(state: :active).order(:exemption_id)
@@ -17,7 +12,7 @@ class ConfirmationLetterPresenter
 
   # Used only if the page contains a single letter.
   def web_page_title
-    t("web_page_title", reg_number: registration.reference)
+    t("web_page_title", reg_number: reference)
   end
 
   def date_of_letter
@@ -25,49 +20,49 @@ class ConfirmationLetterPresenter
   end
 
   def submission_date
-    registration.submitted_at.to_date.to_formatted_s(:day_month_year)
+    submitted_at.to_date.to_formatted_s(:day_month_year)
   end
 
   # Provides the full postal address for the letter.
   def postal_address_lines
     [
-      "#{registration.contact_first_name} #{registration.contact_last_name}",
-      registration.operator_name,
-      address_lines(registration.contact_address)
+      "#{contact_first_name} #{contact_last_name}",
+      operator_name,
+      address_lines(contact_address)
     ].flatten!.reject(&:blank?)
   end
 
-  def registration_completed_by_items
-    applicant_full_name = "#{registration.applicant_first_name} #{registration.applicant_last_name}"
+  def reg_completed_by_items
+    applicant_full_name = "#{applicant_first_name} #{applicant_last_name}"
     filter_blank_items([
-                         { key: t("registration_completed_by.name"), value: applicant_full_name },
-                         { key: t("registration_completed_by.telephone"), value: registration.applicant_phone },
-                         { key: t("registration_completed_by.email"), value: registration.applicant_email }
+                         { key: t("reg_completed_by_name"), value: applicant_full_name },
+                         { key: t("reg_completed_by_telephone"), value: applicant_phone },
+                         { key: t("reg_completed_by_email"), value: applicant_email }
                        ])
   end
 
   def business_details_items
     filter_blank_items([
-      { key: t("business_details.type"), value: I18n.t(registration.business_type, scope: "organisation_type") },
-      registration.business_type == "partnership" ? list_of_people : business_details
+      { key: t("business_details.type"), value: I18n.t(business_type, scope: "organisation_type") },
+      business_type == "partnership" ? list_of_people : business_details
     ].flatten)
   end
 
   def waste_operation_contact_items
-    contact_full_name = "#{registration.contact_first_name} #{registration.contact_last_name}"
+    contact_full_name = "#{contact_first_name} #{contact_last_name}"
     filter_blank_items([
                          { key: t("waste_operation_contact.name"), value: contact_full_name },
-                         { key: t("waste_operation_contact.position"), value: registration.contact_position },
-                         { key: t("waste_operation_contact.telephone"), value: registration.contact_phone },
-                         { key: t("waste_operation_contact.email"), value: registration.contact_email }
+                         { key: t("waste_operation_contact.position"), value: contact_position },
+                         { key: t("waste_operation_contact.telephone"), value: contact_phone },
+                         { key: t("waste_operation_contact.email"), value: contact_email }
                        ])
   end
 
   def waste_operation_location_items
     filter_blank_items([
-                         { key: t("waste_operation_location.address"), value: address_lines(registration.site_address).join(", ") },
-                         { key: t("waste_operation_location.ngr"), value: registration.site_address.grid_reference },
-                         { key: t("waste_operation_location.details"), value: registration.site_address.description }
+                         { key: t("waste_operation_location.address"), value: address_lines(site_address).join(", ") },
+                         { key: t("waste_operation_location.ngr"), value: site_address.grid_reference },
+                         { key: t("waste_operation_location.details"), value: site_address.description }
                        ])
   end
 
@@ -93,7 +88,7 @@ class ConfirmationLetterPresenter
   attr_reader :registration
 
   def registration_exemptions_with_exemptions
-    registration.registration_exemptions.includes(:exemption)
+    registration_exemptions.includes(:exemption)
   end
 
   def t(key, options = {})
@@ -112,7 +107,7 @@ class ConfirmationLetterPresenter
   end
 
   def list_of_people
-    registration.people.each_with_index.map do |person, index|
+    people.each_with_index.map do |person, index|
       {
         key: t(".business_details.partner_enumerator", count: index + 1),
         value: ["#{person.first_name} #{person.last_name}"]
@@ -121,15 +116,15 @@ class ConfirmationLetterPresenter
   end
 
   def business_details
-    org_type = registration.business_type.underscore
+    org_type = business_type.underscore
     org_type = "default" unless %w[limited_company limited_liability_partnership].include? org_type
     org_name_key = "business_details.name_#{org_type}"
     org_number_key = "business_details.number_#{org_type}"
     org_address_key = "business_details.address_#{org_type}"
-    operator_address = address_lines(registration.operator_address).join(", ")
+    operator_address = address_lines(operator_address).join(", ")
     [
-      { key: t(org_name_key), value: registration.operator_name },
-      { key: t(org_number_key), value: registration.company_no },
+      { key: t(org_name_key), value: operator_name },
+      { key: t(org_number_key), value: company_no },
       { key: t(org_address_key), value: operator_address }
     ]
   end
