8fea0c687fa4c05362131bc5144f6d09e8885dad
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2019-05-17 15:20:53 +0100

    Rename to presenter in the view
    I believe its a more commonly recognised pattern to name a presenter `presenter` in the view.

--- app/controllers/confirmation_letter_controller.rb
+++ app/controllers/confirmation_letter_controller.rb
@@ -10,7 +10,7 @@ class ConfirmationLetterController < ApplicationController
     render pdf: registration.reference,
            show_as_html: show_as_html,
            layout: false,
-           locals: { confirmation_letter_data: ConfirmationLetterPresenter.new(registration) },
+           locals: { presenter: ConfirmationLetterPresenter.new(registration, view_context) },
            page_size: "A4",
            margin: { top: "10mm", bottom: "10mm", left: "10mm", right: "10mm" },
            print_media_type: true
