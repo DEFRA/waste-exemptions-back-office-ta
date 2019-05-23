961cac01f942ac0037ea32a431a5293192722c39
cintamani <cintamani.puddu@gmail.com>
2019-05-23 12:33:56 +0100

    Remove wickedpdf gem and configs
    part of: https://eaflood.atlassian.net/browse/RUBY-223
    
    The wicked_pdf gem, initializer config and script are already part of the engine code and dependency of this application.
    Requiring and installing the gem twice was causing an error generation in the PDF recorded in the ticket above, for which a PDF was generated with wrong content.

--- config/initializers/wicked_pdf.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require "wicked_pdf"
-
-if WasteExemptionsEngine.configuration.use_xvfb_for_wickedpdf
-  WickedPdf.config = {
-    exe_path: WasteExemptionsEngine::Engine.root.join("script", "wkhtmltopdf.sh").to_s
-  }
-end
