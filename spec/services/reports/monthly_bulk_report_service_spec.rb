ef9fddac6347ede7efeb1ce2fafc3b416df9bed8
cintamani <cintamani.puddu@gmail.com>
2019-06-07 13:55:46 +0100

    Work around database cleaner not clening the DB properly

--- spec/services/reports/monthly_bulk_report_service_spec.rb
+++ spec/services/reports/monthly_bulk_report_service_spec.rb
@@ -13,9 +13,12 @@ module Reports
 
           # Expect no error gets notified
           expect(Airbrake).to_not receive(:notify)
-          expect(GeneratedReport.count).to be_zero
 
-          MonthlyBulkReportService.run(first_day_of_the_month)
+          expect {
+            MonthlyBulkReportService.run(first_day_of_the_month)
+          }.to change {
+            GeneratedReport.count
+          }.by(1)
 
           expect(GeneratedReport.last.file_name).to eq("20190601-20190630.csv")
         end
