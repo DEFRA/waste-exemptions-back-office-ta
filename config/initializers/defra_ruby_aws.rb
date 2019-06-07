da5a9c19de3213978b95a21459a8a2c4940eec33
cintamani <cintamani.puddu@gmail.com>
2019-06-07 11:51:10 +0100

    Remove validations on ENV variables

--- config/initializers/defra_ruby_aws.rb
+++ config/initializers/defra_ruby_aws.rb
@@ -3,16 +3,8 @@
 require "defra_ruby/aws"
 
 DefraRuby::Aws.configure do |c|
-  def raise_missing_env_var(variable)
-    raise("Environment variable #{variable} has not been set")
-  end
-
-  ENV["AWS_BULK_EXPORT_BUCKET"].present? || raise_missing_env_var("AWS_BULK_EXPORT_BUCKET")
-  ENV["AWS_BULK_EXPORT_ACCESS_KEY_ID"].present? || raise_missing_env_var("AWS_BULK_EXPORT_ACCESS_KEY_ID")
-  ENV["AWS_BULK_EXPORT_SECRET_ACCESS_KEY"] || raise_missing_env_var("AWS_BULK_EXPORT_SECRET_ACCESS_KEY")
-
   c.buckets = [{
-    name: (ENV["AWS_BULK_EXPORT_BUCKET"] || raise_missing_env_var("AWS_BULK_EXPORT_BUCKET")),
+    name: ENV["AWS_BULK_EXPORT_BUCKET"],
     region: ENV["AWS_REGION"],
     credentials: {
       access_key_id: ENV["AWS_BULK_EXPORT_ACCESS_KEY_ID"],
