f186f723cbf3f32a28e4a81f628127c70aadb113
cintamani <cintamani.puddu@gmail.com>
2019-06-10 16:47:57 +0100

    Update generated_report.rb

--- app/models/reports/generated_report.rb
+++ app/models/reports/generated_report.rb
@@ -5,9 +5,5 @@ require "defra_ruby/exporters"
 module Reports
   class GeneratedReport < ActiveRecord::Base
     self.table_name = :reports_generated_reports
-
-    def starts_from
-      Date.parse(file_name.split("-").first, :plain_year_month_day)
-    end
   end
 end
