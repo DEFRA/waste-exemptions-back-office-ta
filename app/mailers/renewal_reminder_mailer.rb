7c5260d06dec60385092fe1f8034f254f832cd28
irisfaraway <iris.faraway@gmail.com>
2019-06-20 15:24:29 +0100

    Set up mailer
    These helper methods still need some work, but at least allow us to send an email.

---
+++ app/mailers/renewal_reminder_mailer.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+class RenewalReminderMailer < ActionMailer::Base
+  def first_reminder_email(registration)
+    @contact_name = contact_name(registration)
+    @expiry_date = expiry_date(registration)
+    @reference = reference(registration)
+    @site_location = site_location(registration)
+    @exemptions = exemptions(registration)
+
+    mail(
+      to: registration.contact_email,
+      from: from_email,
+      subject: I18n.t(".renewal_reminder_mailer.first_reminder_email.subject")
+    )
+  end
+
+  private
+
+  def from_email
+    config = WasteExemptionsEngine.configuration
+    "#{config.service_name} <#{config.email_service_email}>"
+  end
+
+  def contact_name(_registration)
+    "TODO"
+  end
+
+  def expiry_date(_registration)
+    "TODO"
+  end
+
+  def reference(_registration)
+    "TODO"
+  end
+
+  def site_location(_registration)
+    "TODO"
+  end
+
+  def exemptions(_registration)
+    ["TODO"]
+  end
+end
---
+++ spec/mailers/renewal_mailer_spec.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe RenewalReminderMailer, type: :mailer do
+  describe "test_email" do
+    before do
+      allow(WasteExemptionsEngine.configuration).to receive(:email_service_email).and_return("wex@example.com")
+      allow(WasteExemptionsEngine.configuration).to receive(:service_name).and_return("WEX")
+    end
+
+    let(:registration) { build(:registration) }
+    let(:mail) { RenewalReminderMailer.first_reminder_email(registration) }
+
+    it "uses the correct 'to' address" do
+      expect(mail.to).to eq([registration.contact_email])
+    end
+
+    it "uses the correct 'from' address" do
+      expect(mail.from).to eq(["wex@example.com"])
+    end
+
+    it "uses the correct subject" do
+      subject = "Your waste exemptions expire soon, register again to continue your waste operations"
+      expect(mail.subject).to eq(subject)
+    end
+
+    it "includes the correct template in the body" do
+      expect(mail.body.encoded).to include("Until our online renewal service is launched, you’ll need to register your exemptions again.")
+    end
+  end
+end
