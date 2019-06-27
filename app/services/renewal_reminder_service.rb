0c981333838667465c72aee20f8edaf21853dfa0
cintamani <cintamani.puddu@gmail.com>
2019-06-27 16:03:21 +0100

    Add error catching and make expire on week configurable

--- app/services/renewal_reminder_service.rb
+++ app/services/renewal_reminder_service.rb
@@ -3,18 +3,31 @@
 class RenewalReminderService < ::WasteExemptionsEngine::BaseService
   def run
     expiring_registrations.each do |registration|
-      RenewalReminderMailer.first_reminder_email(registration).deliver_now
+      begin
+        RenewalReminderMailer.first_reminder_email(registration).deliver_now
+      rescue StandardError => e
+        Airbrake.notify e, registration: registration.reference
+        Rails.logger.error "Failed to send first renewal email for registration #{registration.reference}"
+      end
     end
   end
 
   private
 
   def expiring_registrations
-    WasteExemptionsEngine::Registration.where(id:
-      WasteExemptionsEngine::RegistrationExemption
-        .all_active_exemptions
-        .where(expires_on: 4.weeks.from_now.to_date)
-        .pluck(:registration_id)
-    )
+    WasteExemptionsEngine::Registration.where(
+      id: all_active_exemptions_registration_ids
+    ).where("contact_email != waste-exemptions@environment-agency.gov.uk")
+  end
+
+  def all_active_exemptions_registration_ids
+    WasteExemptionsEngine::RegistrationExemption
+      .all_active_exemptions
+      .where(expires_on: expires_in_weeks.weeks.from_now.to_date)
+      .pluck(:registration_id)
+  end
+
+  def expires_in_weeks
+    WasteExemptionsBackOffice::Application.config.first_renewal_email_reminder_weeks.to_i
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -61,6 +61,7 @@ module WasteExemptionsBackOffice
 
     # Emails
     config.email_test_address = ENV["EMAIL_TEST_ADDRESS"]
+    config.first_renewal_email_reminder_weeks = ENV["FIRST_RENEWAL_EMAIL_REMINDER_WEEKS"]
 
     # Version info
     config.application_name = "waste-exemptions-back-office-ta"
