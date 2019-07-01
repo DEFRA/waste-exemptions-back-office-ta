c9c7cf83c448d65c3e41935113e93de2b4dfa598
cintamani <cintamani.puddu@gmail.com>
2019-07-01 12:10:03 +0100

    Do not seed exemptions in the test suite, use factories instead
    This removes the need of seeding Exemption in the database and fixes connected specs.
    Housekeeping and alignment with other services.

--- spec/models/reports/monthly_bulk_serializer_spec.rb
+++ spec/models/reports/monthly_bulk_serializer_spec.rb
@@ -11,14 +11,12 @@ module Reports
     describe "#to_csv" do
       it "return a string in CSV format with complete exemptions details for the given month" do
         create_list(:registration, 3)
-
-        # TODO: Solve this at a factory level and deal with the cascade of fixes
-        WasteExemptionsEngine::RegistrationExemption.update_all(registered_on: Date.today)
-
+        # The registration factories default to creating 3 exemptions per-registration factory.
+        total_exemptions = 3 * 3
         csv_lines = monthly_bulk_serializer.to_csv.split("\n")
 
         expect(csv_lines.first).to eq(described_class::ATTRIBUTES.map(&:to_s).join(","))
-        expect(csv_lines.count).to eq(WasteExemptionsEngine::RegistrationExemption.count + 1)
+        expect(csv_lines.count).to eq(total_exemptions + 1)
       end
     end
   end
