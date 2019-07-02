4347b2141dac8bd039c0ed18a1dd9465bd2f9c99
cintamani <cintamani.puddu@gmail.com>
2019-07-02 13:31:41 +0100

    Schedule the registration exemptions expiry job to run daily
    From https://eaflood.atlassian.net/browse/RUBY-413
    
    Add configuration and scheduling of the registration exemptions expiry job

--- config/schedule.rb
+++ config/schedule.rb
@@ -35,3 +35,11 @@ end
 every :day, at: (ENV["FIRST_RENEWAL_EMAIL_REMINDER_DAILY_RUN_TIME"] || "1:05"), roles: [:db] do
   rake "email:renew_reminder:first:send"
 end
+
+# This is the registration exemptions exiry job which will collect all active
+# registration exemptions that have an expire date in the past and will set their
+# state to `expired`
+bulk_time = (ENV["EXPIRE_REGISTRATION_EXEMPTION_RUN_TIME"] || "20:05")
+every :day, at: bulk_time, roles: [:db] do
+  rake "expire_registration:run"
+end
