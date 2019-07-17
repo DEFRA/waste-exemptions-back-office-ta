fdde43ec5613619eef1843034738955e6040a402
cintamani <cintamani.puddu@gmail.com>
2019-07-17 10:35:13 +0100

    Add test coverage for Codeclimate

---
+++ spec/models/boxi_export/base_serializer_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+module BoxiExport
+  RSpec.describe BaseSerializer do
+    describe "#file_name" do
+      it "raises a not implemented error" do
+        expect { subject.file_name }.to raise_error(NotImplementedError)
+      end
+    end
+
+    describe "#records_scope" do
+      it "raises a not implemented error" do
+        expect { subject.records_scope }.to raise_error(NotImplementedError)
+      end
+    end
+  end
+end
