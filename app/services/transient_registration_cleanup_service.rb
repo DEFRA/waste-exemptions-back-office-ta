00cdc435a6a9231e696a39fcafd4bb5d4adaaae4
Iris Faraway <iris.faraway@gmail.com>
2019-08-23 11:08:55 +0100

    Add rake task to remove old transient_registrations (#350)
    https://eaflood.atlassian.net/browse/RUBY-326
    
    We've agreed that we shouldn't store transient_registrations for more than 30 days. So we need a way of clearing out the old ones.
    
    This rake task will find and remove transient_registrations that are older than 30 days. It should also remove all related objects (eg transient_addresses).

---
+++ app/services/transient_registration_cleanup_service.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+class TransientRegistrationCleanupService < ::WasteExemptionsEngine::BaseService
+  def run
+    transient_registrations_to_remove.destroy_all
+  end
+
+  private
+
+  def transient_registrations_to_remove
+    WasteExemptionsEngine::TransientRegistration.where("created_at < ?", oldest_possible_date)
+  end
+
+  def oldest_possible_date
+    max = Rails.configuration.max_transient_registration_age_days
+    max.days.ago
+  end
+end
