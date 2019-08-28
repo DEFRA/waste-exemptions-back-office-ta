a37eb249d07b4d44e22625be4b9470b712acc3ac
irisfaraway <iris.faraway@gmail.com>
2019-08-28 16:08:39 +0100

    Display expiry date in first paragraph

--- app/presenters/renewal_letter_presenter.rb
+++ app/presenters/renewal_letter_presenter.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
 class RenewalLetterPresenter < BaseLetterPresenter
+  def expiry_date
+    # Currently you can only add exemptions when you register, so we can assume they expire at the same time
+    registration_exemptions.first.expires_on.to_formatted_s(:day_month_year)
+  end
 end
--- spec/presenters/renewal_letter_presenter_spec.rb
+++ spec/presenters/renewal_letter_presenter_spec.rb
@@ -3,5 +3,16 @@
 require "rails_helper"
 
 RSpec.describe RenewalLetterPresenter do
+  let(:registration) { create(:registration, :with_active_exemptions) }
+  subject { described_class.new(registration) }
+
   it_behaves_like "a letter presenter"
+
+  describe "#expiry_date" do
+    it "returns the registration's expiry_date formatted as for example '2 April 2019'" do
+      parsed_date = Date.parse(subject.expiry_date)
+
+      expect(parsed_date).to eq(registration.registration_exemptions.first.expires_on)
+    end
+  end
 end
