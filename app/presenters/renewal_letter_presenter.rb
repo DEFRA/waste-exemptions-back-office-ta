648f23000132186824737e756955fac9adbb72f6
irisfaraway <iris.faraway@gmail.com>
2019-08-28 17:04:00 +0100

    List first 20 exemptions
    And display a message if there are some that can't be listed.

--- app/presenters/renewal_letter_presenter.rb
+++ app/presenters/renewal_letter_presenter.rb
@@ -1,8 +1,33 @@
 # frozen_string_literal: true
 
 class RenewalLetterPresenter < BaseLetterPresenter
+  def exemption_description(exemption)
+    "#{exemption.code} #{exemption.summary}"
+  end
+
   def expiry_date
     # Currently you can only add exemptions when you register, so we can assume they expire at the same time
     registration_exemptions.first.expires_on.to_formatted_s(:day_month_year)
+  end
+
+  def listable_exemptions
+    @_listable_exemptions = exemptions.first(20)
+  end
+
+  def unlisted_exemption_count
+    @_unlisted_exemption_count = calculate_number_of_unlisted_exemptions
+  end
+
+  private
+
+  def calculate_number_of_unlisted_exemptions
+    total_exemption_count = exemptions.count
+    listable_exemptions_count = listable_exemptions.count
+
+    if listable_exemptions_count < total_exemption_count
+      total_exemption_count - listable_exemptions_count
+    else
+      0
+    end
   end
 end
--- spec/presenters/renewal_letter_presenter_spec.rb
+++ spec/presenters/renewal_letter_presenter_spec.rb
@@ -15,4 +15,49 @@ RSpec.describe RenewalLetterPresenter do
       expect(parsed_date).to eq(registration.registration_exemptions.first.expires_on)
     end
   end
+
+  describe "#exemption_description" do
+    it "returns the exemption code and summary in the correct format" do
+      exemption = build(:exemption)
+      string = "#{exemption.code} #{exemption.summary}"
+
+      expect(subject.exemption_description(exemption)).to eq(string)
+    end
+  end
+
+  describe "#listable_exemptions" do
+    context "when there are 20 or fewer exemptions" do
+      it "returns all exemptions" do
+        expect(subject.listable_exemptions).to eq(registration.exemptions)
+      end
+    end
+
+    context "when there are more than 20 exemptions" do
+      before do
+        registration.exemptions = build_list(:exemption, 21)
+      end
+
+      it "only returns the first 20" do
+        expect(subject.listable_exemptions).to eq(registration.exemptions.first(20))
+      end
+    end
+  end
+
+  describe "#unlisted_exemption_count" do
+    context "when there are 20 or fewer exemptions" do
+      it "returns 0" do
+        expect(subject.unlisted_exemption_count).to eq(0)
+      end
+    end
+
+    context "when there are more than 20 exemptions" do
+      before do
+        registration.exemptions = build_list(:exemption, 21)
+      end
+
+      it "returns the count minus 20" do
+        expect(subject.unlisted_exemption_count).to eq(1)
+      end
+    end
+  end
 end
