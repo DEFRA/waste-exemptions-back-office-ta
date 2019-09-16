bda10e9c849090f02334fe1d9b16ef94c59773a1
cintamani <cintamani.puddu@gmail.com>
2019-09-16 10:23:40 +0100

    Fix all the Ss

--- app/controllers/ad_renewal_letters_exports_controller.rb
+++ app/controllers/ad_renewal_letters_exports_controller.rb
@@ -8,14 +8,14 @@ class AdRenewalLettersExportsController < ApplicationController
   end
 
   def update
-    @ad_renewal_letters_exports = WasteExemptionsEngine::AdRenewalLettersExport.find(params[:id])
+    ad_renewal_letters_export = WasteExemptionsEngine::AdRenewalLettersExport.find(params[:id])
 
-    @ad_renewal_letters_exports.update_attributes(ad_renewal_letters_exports_attributes)
+    ad_renewal_letters_export.update_attributes(ad_renewal_letters_export_attributes)
 
     redirect_to ad_renewal_letters_exports_path
   end
 
-  def ad_renewal_letters_exports_attributes
+  def ad_renewal_letters_export_attributes
     params.require(:ad_renewal_letters_export).permit(:printed_on, :printed_by)
   end
 end
