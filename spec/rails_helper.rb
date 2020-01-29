b4b7ac0e1dc4277226d4acae65800bb33895755c
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2020-01-29 13:09:04 +0000

    Update VCR cassettes (#487)
    Every couple of weeks we update our VCR cassettes to ensure our mock requests match the current real-world ones.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,7 +4,6 @@
 require "spec_helper"
 ENV["RAILS_ENV"] ||= "test"
 require File.expand_path("../config/environment", __dir__)
-Dir["./lib/defra_ruby/**/*.rb"].each { |file| require file }
 
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
